7c8cce8949413aee497dc1e5e37d80a09579232a
nortonr <nortonrodriguez@gmail.com>
2019-06-23 20:56:43 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-markdown',
+        name: 'gym',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
